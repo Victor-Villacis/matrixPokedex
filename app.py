1086d155aacc2f8e2732630d56786c5c07d590a6
Victor Villacis <victor.villacis@outlook.com>
2026-04-01 15:24:01 +0000

    Pending changes exported from your codespace

--- app.py
+++ app.py
@@ -3,5 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+def pokedex_home():
+    return render_template("index.html", title="Pokedex Explorer")
+
+if __name__ == "__main__":
+    app.run(debug=True)
