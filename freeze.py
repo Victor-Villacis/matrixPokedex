8ccc5e70b87ff66ba29d494ce27c6aa5034ce140
Victor Villacis <victor.villacis@outlook.com>
2026-04-01 16:18:41 +0000

    Fix URLs for GitHub Pages with base tag and relative paths

--- freeze.py
+++ freeze.py
@@ -3,8 +3,6 @@ from app import app
 
 app.config['FREEZER_DESTINATION'] = 'build'
 app.config['FREEZER_RELATIVE_URLS'] = True
-# Use the final public URL path for GitHub Pages deployment.
-app.config['FREEZER_BASE_URL'] = 'https://victor-villacis.github.io/matrixPokedex/'
 
 freezer = Freezer(app)
 
